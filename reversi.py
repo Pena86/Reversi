e55f9b23cc5a4c31a5fcd8d6001d5b2703118ea0
Eero Penttilä <epenttil@paju.oulu.fi>
2014-01-25 09:59:58 +0200

    Move validation methods

--- reversi.py
+++ reversi.py
@@ -45,14 +45,67 @@ class Reversi:
     def moveTo(self, x, y):
         """Method for players to call when making a move in the game
         """
-        if x != None and y != None:
-            self.board[x][y] = self.player
+        if x != None and y != None and self.board[x][y] == 0 and self.validMove(x, y, self.player):
             self.turn += 1
             self.ui()
+            return True
 
-        return True
+        return False
 
     def abort(self):
+        """If the game needs to be aborted at the middle
+        """
         self.run = False
         self.player1.gameEnd()
         self.player2.gameEnd()
+
+    def validMove(self, x, y, player):
+        """If the move is valid, it's made to the board and return True
+            else return False
+        """
+        if player == 1:
+            opponent = 2
+        elif player == 2:
+            opponent = 1
+        else:
+            print ("No player")
+            return False
+        #print (player, opponent, x, y)
+
+        piecesToFlip = []
+        directionsToCheck = [[0,-1],[1,-1],[1,0],[1,1],[0,1],[-1,1],[-1,0],[-1,-1]]
+        for direction in directionsToCheck:
+            piecesToFlip.extend(self.checkDirection(x, y, direction[0], direction[1], player, opponent))
+        #print (piecesToFlip)
+
+        if len(piecesToFlip) > 0:
+            self.board[x][y] = player
+            for spot in piecesToFlip:
+                self.board[spot[0]][spot[1]] = player
+            return True
+
+
+        return False
+
+    def checkDirection(self, x, y, dx, dy, player, opponent):
+        """Checks pieces to flip in the given ridection and return a list of tuples for those pieces to flip.
+            else return empty list
+        """
+        x += dx
+        y += dy
+        foundOpp = []
+        while x < 8 and x > -1 and y < 8 and y > -1:
+            #print (x, y, len(foundOpp))
+            if self.board[x][y] == opponent:
+                foundOpp.append([x,y])
+                #print (foundOpp)
+            elif len(foundOpp) > 0 and self.board[x][y] == player:
+                #print ("found own")
+                return foundOpp
+            else:
+                return []
+
+            x += dx
+            y += dy
+            pass
+        return []
